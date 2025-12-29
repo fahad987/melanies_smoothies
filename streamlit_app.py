649a3e511431e7e9da73c62bc3fcc51eb39f18ab
Fahad Rehman <fahad.rehman365@yahoo.com>
2025-12-29 16:13:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ if name_on_order:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
